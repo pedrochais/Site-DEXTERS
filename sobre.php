b0d1544eb27775583438cec8144bb7b2fc7c35e7
gabrielsilva28 <go.silva@discente.ufma.br>
2022-02-06 01:34:13 -0300

    SOBRE atualizado

---
+++ sobre.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="images/icone.png">
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="style/sobre.css">
+    <title>DEXTERS | Sobre</title>
+</head>
+<body>
+    <?php include('cabecalho.html'); ?>
+
+    <main>
+        <section id="sobre">
+            <div class="espaço">
+                <div class="titulosobre">
+                    <h2>Sobre Nós</h2> 
+                </div>
+                <p align="justify">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <p align="justify">
+                    The purpose of lorem ipsum is to create a natural looking block of text (sentence, paragraph, page, etc.) that doesn't distract from the layout. A practice not without controversy, laying out pages with meaningless filler text can be very useful when the focus is meant to be on design, not content.
+                    The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software. Today it's seen all around the web; on templates, websites, and stock designs. Use our generator to get your own, or read on for the authoritative history of lorem ipsum.
+
+                </p>
+                <p align="justify">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    
+                </p>
+
+
+
+
+
+                <section id="imagens">
+                    <div class="galeria">
+                        <img src="./images/logo_dexters_header.png" alt="">
+                        <img src="./images/logo_dexters_header.png" alt="">
+                        <img src="./images/logo_dexters_header.png" alt="">
+                        <img src="./images/logo_dexters_header.png" alt="">
+                    </div>
+                </section>
+
+            </div>
+        
+
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </section>
+        
+
+
+
+
+
+    </main>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <?php include('rodape.html'); ?>
+</body>
+</html>
