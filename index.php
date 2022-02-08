9b03c272c08021c94cc315712f3464e8329ff34e
IA-V <iagoviper48@gmail.com>
2022-02-08 08:43:18 -0300

    Att home, footer, header e pagina de integrantes

--- index.php
+++ index.php
@@ -21,6 +21,45 @@
                 </div>
             </div>
         </section>
+
+    <section id="atividades">
+      <div class="atividades">
+        <div class="atividades-desenvolvidas">
+          <h1>Áreas de Atuação</h1>
+          <p>
+            O nosso laboratório trabalha com as mais diversas áreas dentro da Engenharia de Software 
+          </p>
+        </div>
+        <div class="images-atividades">
+          <div class="card-atividade-1">
+            <div class="texto">
+              <h3>Engenharia de Software</h3>
+            </div>
+          </div>
+          <div class="card-atividade-2">
+            <div class="texto">
+              <h3>Modelagem de Sistemas Complexos</h3>
+            </div>
+          </div>
+          <div class="card-atividade-3">
+            <div class="texto">
+              <h3>Interação Humano Computador</h3>
+            </div>
+          </div>
+          <div class="card-atividade-4">
+            <div class="texto">
+              <h3>Sistemas de Informação</h3>
+            </div>
+          </div>
+          <div class="card-atividade-5">
+            <div class="texto">
+              <h3>Ensino de Computação</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
         <section id="conheca-dexters">
             <h2>Quem somos?</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas suscipit elit ligula, et gravida massa eleifend non. Vestibulum tempus feugiat justo, ornare ornare ex auctor vel. Curabitur interdum nisi ut augue tempus consequat. Phasellus nec tempus tortor. Nullam erat risus, consectetur accumsan orci sit amet, hendrerit egestas ante. Pellentesque est lectus, rutrum sit amet tincidunt a, dictum in enim. Sed commodo risus neque, sed commodo mi ultricies ultrices. Sed vitae elementum est. Duis nec lectus mauris. Nam sed orci neque.</p>
