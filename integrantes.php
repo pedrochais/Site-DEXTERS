5a74348d8d4c9efd6bc07cc09789662e12d5a450
Pedro Rocha <pedrorocha.bou@gmail.com>
2022-02-09 12:19:06 -0300

    desenvolvendo modal pagina integrantes

--- integrantes.php
+++ integrantes.php
@@ -19,26 +19,31 @@
             <div class="card-integrante">
                 <div class="imagem"></div>
                 <h4>Nome Sobrenome</h4>
+                <button onclick="iniciaModal()" class="btn-detalhes">Detalhes</button>
             </div>
 
             <div class="card-integrante">
                 <div class="imagem"></div>
                 <h4>Nome Sobrenome</h4>
+                <button class="btn-detalhes">Detalhes</button>
             </div>
 
             <div class="card-integrante">
                 <div class="imagem"></div>
                 <h4>Nome Sobrenome</h4>
+                <button class="btn-detalhes">Detalhes</button>
             </div>
 
             <div class="card-integrante">
                 <div class="imagem"></div>
                 <h4>Nome Sobrenome</h4>
+                <button class="btn-detalhes">Detalhes</button>
             </div>
 
             <div class="card-integrante">
                 <div class="imagem"></div>
                 <h4>Nome Sobrenome</h4>
+                <button class="btn-detalhes">Detalhes</button>
             </div>
 
         </section>
@@ -74,5 +79,25 @@
     </main>
     
     <?php include('rodape.html'); ?>
+
+    <!-- MODAL -->
+    <div id="modal-integrantes" class="modal-background show">
+        <div class="modal">
+            <button onclick="fechaModal()" class="btn-fechar">x</button>
+            <div class="bg-titulo">
+                <h3>Informações do integrante</h3>
+            </div>
+            
+            <div class="infos">
+                <p>Nome: </p>
+                <p>Cargo: </p>
+                <p>Lattes: </p>
+                <p>Contato: </p>
+            </div>
+            
+        </div>
+    </div>
+
+    <script src="script/integrantes.js"></script>
 </body>
 </html>
