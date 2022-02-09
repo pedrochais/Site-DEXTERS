9ba97a8964b80e3c20afba3b5d701e0b3f075c36
André Filipe Sousa  Barreto <barreto.andre@discente.ufma.br>
2022-02-09 12:20:15 -0300

    pag biblioteca -não finalizada

---
+++ biblioteca.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="images/icone.png">
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="style/biblioteca.css">
+    <title>DEXTERS</title>
+</head>
+<body>
+    <?php include('cabecalho.html'); ?>
+    <main>
+    <section id="biblioteca">
+      <div class="section-header">
+        <h2 class="titulo">BIBLIOTECA</h2>
+      </div>
+      <details>
+          <summary>
+              <div class="nome">
+                  Projeto 1
+                  <span>Detalhes</span>
+              </div>
+              <div class="resumo">
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores alias, explicabo itaque quia minus expedita quam qui fuga doloremque veniam necessitatibus sint repellendus vero, adipisci placeat ratione, aut recusandae beatae.
+              </div>
+          </summary>
+          <div class="descricao">
+            <ul>
+                <li><img src="images/logo_UFMA.png" alt=""></li>
+                <li><h3>Descrição do projeto</h3>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente, soluta voluptates repellat dicta, corrupti aliquid at minima tempore fugiat voluptatibus fugit, pariatur eum illo dignissimos molestiae perferendis ex reprehenderit iste.</li>
+            </ul>
+          </div>
+      </details>
+
+      <details>
+          <summary>
+              <div class="nome">
+                  Projeto 2
+                  <span>Detalhes</span>
+              </div>
+              <div class="resumo">
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores alias, explicabo itaque quia minus expedita quam qui fuga doloremque veniam necessitatibus sint repellendus vero, adipisci placeat ratione, aut recusandae beatae.
+              </div>
+          </summary>
+          <div class="descricao">
+            <ul>
+                <li><img src="images/logo_UFMA.png" alt=""></li>
+                <li><h3>Descrição do projeto</h3>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente, soluta voluptates repellat dicta, corrupti aliquid at minima tempore fugiat voluptatibus fugit, pariatur eum illo dignissimos molestiae perferendis ex reprehenderit iste.</li>
+            </ul>
+          </div>
+      </details>
+
+      <details>
+          <summary>
+              <div class="nome">
+                  Projeto 3
+                  <span>Detalhes</span>
+              </div>
+              <div class="resumo">
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores alias, explicabo itaque quia minus expedita quam qui fuga doloremque veniam necessitatibus sint repellendus vero, adipisci placeat ratione, aut recusandae beatae.
+              </div>
+          </summary>
+          <div class="descricao">
+            <ul>
+                <li><img src="images/logo_UFMA.png" alt=""></li>
+                <li><h3>Descrição do projeto</h3>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente, soluta voluptates repellat dicta, corrupti aliquid at minima tempore fugiat voluptatibus fugit, pariatur eum illo dignissimos molestiae perferendis ex reprehenderit iste.</li>
+            </ul>
+          </div>
+      </details>
+
+      <details>
+          <summary>
+              <div class="nome">
+                  Projeto 4
+                  <span>Detalhes</span>
+              </div>
+              <div class="resumo">
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores alias, explicabo itaque quia minus expedita quam qui fuga doloremque veniam necessitatibus sint repellendus vero, adipisci placeat ratione, aut recusandae beatae.
+              </div>
+          </summary>
+          <div class="descricao">
+            <ul>
+                <li><img src="images/logo_dexters_header.png" alt=""></li>
+                <li><h3>Descrição do projeto</h3>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente, soluta voluptates repellat dicta, corrupti aliquid at minima tempore fugiat voluptatibus fugit, pariatur eum illo dignissimos molestiae perferendis ex reprehenderit iste.</li>
+            </ul>
+          </div>
+      </details>
+
+
+    </section>
+
+
+
+
+
+
+
+
+    </main>
+   
+        
+    <?php include('rodape.html'); ?>
+</body>
+</html>
