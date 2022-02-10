19e3790d28022ceb5799e2de5c7dfee5cc56eb4d
gabrielsilva28 <go.silva@discente.ufma.br>
2022-02-10 17:21:22 -0300

    atualizacao de noticias e do sobre

---
+++ noticias.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="images/icone.png">
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="style/noticias.css">
+    <title>DEXTERS | Notícias</title>
+</head>
+<body>
+    <?php include('cabecalho.html'); ?>
+    <main>
+        <h2 class="titulo">NOTÍCIAS</h2>
+        <section id="noticias">
+    		<div class="card-item">
+        		<h4>TÍTULO DA NOTÍCIA</h4>
+        		<h4>Descricao da Noticia:</h4>
+        		<h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam posuere pulvinar mi a placerat. Morbi mi lectus, molestie quis ligula vel, dapibus maximus urna. Proin eget enim turpis. Quisque eget pretium arcu. Donec quis massa aliquam, interdum metus sed</h4>
+                <div class="imagem"></div>
+                <div class="btn-novo">
+                	<button onclick="iniciaModal()" class="btn-detalhes">Abrir notícia</button>
+                </div>
+            </div>
+        	
+            <div class="card-item">
+        		<h4>TÍTULO DA NOTÍCIA</h4>
+        		<h4>Descricao da Noticia:</h4>
+        		<h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam posuere pulvinar mi a placerat. Morbi mi lectus, molestie quis ligula vel, dapibus maximus urna. Proin eget enim turpis. Quisque eget pretium arcu. Donec quis massa aliquam, interdum metus sed</h4>
+                <div class="imagem"></div>
+                <div class="btn-novo">
+                	<button onclick="iniciaModal()" class="btn-detalhes">Abrir notícia</button>
+                </div>
+            </div>
+            <div class="card-item">
+        		<h4>TÍTULO DA NOTÍCIA</h4>
+        		<h4>Descricao da Noticia:</h4>
+        		<h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam posuere pulvinar mi a placerat. Morbi mi lectus, molestie quis ligula vel, dapibus maximus urna. Proin eget enim turpis. Quisque eget pretium arcu. Donec quis massa aliquam, interdum metus sed</h4>
+                <div class="imagem"></div>
+                <div class="btn-novo">
+                	<button onclick="iniciaModal()" class="btn-detalhes">Abrir notícia</button>
+                </div>
+            </div>
+
+     	</section>
+    </main>
+
+    <?php include('rodape.html'); ?>
+
+
+</body>
+</html>
+
