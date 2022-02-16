fcbd41e166124abb03450826d2a98fbeb8a4a0ef
Lucas Farias <lucasfariaslfp@gmail.com>
2022-02-16 12:06:32 -0300

    desenvolvendo pagina eventos

---
+++ eventos.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="images/icone.png">
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="style/eventos.css">
+    <title>DEXTERS | Notícias</title>
+</head>
+<body>
+    <?php include('cabecalho.html'); ?>
+    
+    <div class="section-header">     
+        <h2 class="titulo">EVENTOS</h2>
+  </div>
+
+  
+
+
+
+
+
+    <script src="js/index.js"></script>
+    <?php include('rodape.html'); ?>
+</body>
