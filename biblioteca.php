a70dafa5d74d67876423a63e4527b035f0835ec5
IA-V <iagoviper48@gmail.com>
2022-02-18 11:10:30 -0300

    Melhoria dos cards da página 'Biblioteca'

--- biblioteca.php
+++ biblioteca.php
@@ -16,70 +16,63 @@
             <div class="section-header">
                 <h2 class="titulo">BIBLIOTECA</h2>
             </div>
-            <details>
-                <summary>
+            <div class="card-default">
+                <div class="sumario">
                     <div class="nome">
                         Projeto 1
-                        <span>Detalhes</span>
                     </div>
                     <div class="resumo">
                         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores alias, explicabo itaque quia minus expedita quam qui fuga doloremque veniam necessitatibus sint repellendus vero, adipisci placeat ratione, aut recusandae beatae.
                     </div>
-                </summary>
+                    <button class="btn-default">Detalhes</button>
+                </div>
                 <div class="descricao">
-                    <div id="imagem">
-                        <img src="images/lg_UFMA.png" alt="">
-                    </div>
+                    <img class="imagem" src="images/lg_UFMA.png" alt="">
                     <div id="text">
                         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente, soluta voluptates repellat dicta, corrupti aliquid at minima tempore fugiat voluptatibus fugit, pariatur eum illo dignissimos molestiae perferendis ex reprehenderit iste. Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam deserunt deleniti eos maiores, pariatur quia doloribus molestias laborum quis alias nihil voluptates tempore impedit cupiditate, cum dicta, quisquam sapiente eveniet.
                     </div>
                 
                 </div>
-            </details>
+            </div>
 
-            <details>
-                <summary>
+            <div class="card-default">
+                <div class="sumario">
                     <div class="nome">
                         Projeto 2
-                        <span>Detalhes</span>
                     </div>
                     <div class="resumo">
                         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores alias, explicabo itaque quia minus expedita quam qui fuga doloremque veniam necessitatibus sint repellendus vero, adipisci placeat ratione, aut recusandae beatae.
                     </div>
-                </summary>
+                    <button class="btn-default">Detalhes</button>
+                </div>
                 <div class="descricao">
-                    <div id="imagem">
-                        <img src="images/lg_UFMA.png" alt="">
-                    </div>
+                    <img class="imagem" src="images/lg_UFMA.png" alt="">
                     <div id="text">
                         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente, soluta voluptates repellat dicta, corrupti aliquid at minima tempore fugiat voluptatibus fugit, pariatur eum illo dignissimos molestiae perferendis ex reprehenderit iste. Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam deserunt deleniti eos maiores, pariatur quia doloribus molestias laborum quis alias nihil voluptates tempore impedit cupiditate, cum dicta, quisquam sapiente eveniet.
                     </div>
                 
                 </div>
-            </details>
+            </div>
 
-            <details>
-                <summary>
+            <div class="card-default">
+                <div class="sumario">
                     <div class="nome">
                         Projeto 3
-                        <span>Detalhes</span>
                     </div>
                     <div class="resumo">
                         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores alias, explicabo itaque quia minus expedita quam qui fuga doloremque veniam necessitatibus sint repellendus vero, adipisci placeat ratione, aut recusandae beatae.
                     </div>
-                </summary>
+                    <button class="btn-default">Detalhes</button>
+                </div>
                 <div class="descricao">
-                    <div id="imagem">
-                        <img src="images/lg_UFMA.png" alt="">
-                    </div>
+                    <img class="imagem" src="images/lg_UFMA.png" alt="">
                     <div id="text">
                         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente, soluta voluptates repellat dicta, corrupti aliquid at minima tempore fugiat voluptatibus fugit, pariatur eum illo dignissimos molestiae perferendis ex reprehenderit iste. Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam deserunt deleniti eos maiores, pariatur quia doloribus molestias laborum quis alias nihil voluptates tempore impedit cupiditate, cum dicta, quisquam sapiente eveniet.
                     </div>
-                
                 </div>
-            </details>
+            </div>
 
-            <details>
+            <!--<details>
                 <summary>
                     <div class="nome">
                         Projeto 4
@@ -119,9 +112,11 @@
                     </div>
                 
                 </div>
-            </details>
+            </details> -->
         </section>                       
     </main> 
     <?php include('rodape.html'); ?>
+
+    <script src="js/biblioteca.js"></script>
 </body>
 </html>
---
+++ js/biblioteca.js
@@ -0,0 +1,11 @@
+const btn_detalhes = document.querySelectorAll('.btn-default');
+const body = document.querySelector('body');
+const descricao = document.querySelectorAll('.descricao');
+
+btn_detalhes.forEach(function (elemento) {
+    elemento.addEventListener('click', function (evento) {
+        const div_pai = elemento.parentElement.parentElement;
+
+        div_pai.lastElementChild.classList.toggle('show-desc');
+    }); 
+});
