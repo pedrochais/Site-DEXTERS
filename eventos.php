75f9f640b809ed44a695e4606f37aba8d7efb8ad
Pedro Rocha <pedrorocha.bou@gmail.com>
2022-02-21 07:29:19 -0300

    corrigindo responsividade e padronizando código (eventos.php, eventos.css)

--- eventos.php
+++ eventos.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,61 +10,42 @@
     <link rel="stylesheet" href="style/eventos.css">
     <title>DEXTERS | Eventos</title>
 </head>
+
 <body>
     <?php include('cabecalho.html'); ?>
-    
+
     <main>
-        <div class="container">
-            <h2 class="titulo">EVENTOS</h2>
-            <section id="eventos">        
-            </section>
+        <h2 class="titulo">EVENTOS</h2>
+        <section id="eventos">
 
-            <div class="noticias">
-                <div class="evento">
-                    <img src="images/lg_UFMA.png">
-                
-                    <div class="textoMateria">
-                     <h2>EVENTO 1</h2>
-                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde saepe repellendus reprehenderit veritatis et, officia quisquam ducimus accusamus ut ratione. Aspernatur tempore recusandae incidunt porro voluptatem vel deleniti quis velit.</p>
-                    </div>
+            <div class="card-evento">
+                <img src="images/lg_UFMA.png">
+                <div class="texto">
+                    <h3>EVENTO 1</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde saepe repellendus reprehenderit veritatis et, officia quisquam ducimus accusamus ut ratione. Aspernatur tempore recusandae incidunt porro voluptatem vel deleniti quis velit.</p>
                 </div>
             </div>
 
-            <div class="noticias">
-                <div class="evento">
-                    <img src="images/lg_UFMA.png">
-                
-                    <div class="textoMateria">
-                     <h2>EVENTO 2</h2>
-                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde saepe repellendus reprehenderit veritatis et, officia quisquam ducimus accusamus ut ratione. Aspernatur tempore recusandae incidunt porro voluptatem vel deleniti quis velit.</p>
-                    </div>
+            <div class="card-evento">
+                <img src="images/lg_UFMA.png">
+
+                <div class="texto">
+                    <h3>EVENTO 2</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde saepe repellendus reprehenderit veritatis et, officia quisquam ducimus accusamus ut ratione. Aspernatur tempore recusandae incidunt porro voluptatem vel deleniti quis velit.</p>
                 </div>
             </div>
 
-            <div class="noticias">
-                <div class="evento">
-                    <img src="images/lg_UFMA.png">
-                
-                    <div class="textoMateria">
-                     <h2>EVENTO 3</h2>
-                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde saepe repellendus reprehenderit veritatis et, officia quisquam ducimus accusamus ut ratione. Aspernatur tempore recusandae incidunt porro voluptatem vel deleniti quis velit.</p>
-                    </div>
+            <div class="card-evento">
+                <img src="images/lg_UFMA.png">
+
+                <div class="texto">
+                    <h3>EVENTO 3</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde saepe repellendus reprehenderit veritatis et, officia quisquam ducimus accusamus ut ratione. Aspernatur tempore recusandae incidunt porro voluptatem vel deleniti quis velit.</p>
                 </div>
             </div>
 
-            <div class="noticias">
-                <div class="evento">
-                    <img src="images/lg_UFMA.png">
-                
-                    <div class="textoMateria">
-                     <h2>EVENTO 4</h2>
-                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde saepe repellendus reprehenderit veritatis et, officia quisquam ducimus accusamus ut ratione. Aspernatur tempore recusandae incidunt porro voluptatem vel deleniti quis velit.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
+        </section>
     </main>
-    
-    <script src="js/index.js"></script>
+
     <?php include('rodape.html'); ?>
 </body>
