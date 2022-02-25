57ab9669453eebb15c78e52ec6d3cc103cbd8a98
gabrielsilva28 <go.silva@discente.ufma.br>
2022-02-25 16:16:06 -0300

    gerenciador de integrantes

---
+++ gerenciar_integrantes.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="images/ic_dexters.png">
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="style/gerenciar_integrantes.css">
+    <title>DEXTERS | Gerenciamento de Integrantes</title>
+</head>
+<body>
+    <?php include('cabecalho.html'); ?>
+    
+    <main>
+        <h2 class="titulo">Acesso restrito</h2>
+
+        <section id="cadastro">
+            <div class="cadastramento">
+                 <form>
+                    <h2 class="name">Cadastro de Integrantes</h2>
+                    <label for="titulo">Nome</label>
+                    <input type="text" id="nome" placeholder="Digite seu nome">
+                    <label for="titulo">Sobrenome</label>
+                    <input type="text" id="sobrenome" placeholder="Digite seu sobrenome">
+                    <label for="titulo">Lattes</label>
+                    <input type="text" id="lattes" placeholder="Digite o link do seu Lattes">
+                    <label for="titulo">Email</label>
+                    <input type="email" id="email" placeholder="Digite seu email">
+                    <a href="#">
+                        <button class="btn-default">Cadastrar</button>
+                    </a>
+                </form>
+            </div>
+        </section>
+    </main>
+
+    <?php include('rodape.html'); ?>
+
+    
+</body>
+
+</html>
