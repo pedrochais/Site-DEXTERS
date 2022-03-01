13edef5ceb3ea3fe2af6add0a54f9d8cab6771b9
gabrielsilva28 <go.silva@discente.ufma.br>
2022-02-28 22:51:17 -0300

    pagina gerenciar_publicacoes.php

---
+++ gerenciar_publicacoes.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="images/ic_dexters.png">
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="style/gerenciar_publicacoes.css">
+    <title>DEXTERS | Gerenciamento de Publicacoes</title>
+</head>
+<body>
+    <?php include('cabecalho.html'); ?>
+    
+    <main>
+        <h2 class="titulo">Dados da Publicação</h2>
+        <section id="cadastro">
+            <div class="cadastramento">
+                 <form class="form-default">
+                    <div class="form-default" method="get">
+                    <label for="nome">Título da Publicação/Artigo</label>
+                    <input type="text" id="nome" placeholder="Digite o título">
+                    </div>
+                    
+                    <div class="input-box" >
+                        <label for="descricao">Descrição</label>
+                        <textarea placeholder="Digite a descrição" maxlength="200"></textarea>
+                        <div class="caracteres">
+                            <span class="min_num">0</span>
+                            <span class="max_num">/ 200</span>
+                        </div>
+                    </div>
+                    
+                    <div class="input-box" >
+                        <label for="resumo">Resumo</label>
+                        <textarea placeholder="Digite o resumo" maxlength="200"></textarea>
+                        <div class="caracteres">
+                            <span class="min_num">0</span>
+                            <span class="max_num">/ 200</span>
+                        </div>
+                    </div>
+
+                    <div class="form-default" >
+                        <label for="link">Link</label>
+                        <input type="url" id="link" placeholder="Digite o Link">
+                        <label for="arquivo">Arquivo</label>
+                        <a href="#">
+                             
+                            <button class="btn-upload">Selecione um arquivo</button>
+                        </a>
+                    </div>
+                    <br>
+
+                    <a href="#">
+                        <button class="btn-default">Cadastrar</button>
+                    </a>
+                                        
+                </form>
+            </div>
+        </section>
+    </main>
+
+    <?php include('rodape.html'); ?>
+
+
+    <script src="js/gerenciar_publicacoes.js"></script>
+</body>
+
+</html>
