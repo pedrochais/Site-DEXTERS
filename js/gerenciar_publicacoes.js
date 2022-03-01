a950fee9c87f03b416ada9068facdb0cce0a2e04
gabrielsilva28 <go.silva@discente.ufma.br>
2022-03-01 18:44:39 -0300

    ajuste no contador de caracteres

--- js/gerenciar_publicacoes.js
+++ js/gerenciar_publicacoes.js
@@ -1,21 +1,21 @@
-let inputBox = document.querySelector('.input-box'),
-	textArea = inputBox.querySelector('textarea'),
-	minNum = inputBox.querySelector('.min_num');
+let inputBox = document.querySelectorAll('.input-box');
 
-textArea.addEventListener('keyup',()=>{
-	let valLenght=textArea.value.length;
-	minNum.innerHTML=valLenght;
-	valLenght>0 
-		? inputBox.classList.add('active')
-		: inputBox.classList.remove('active');
-
-	valLenght>200
-		? inputBox.classList.add('error')
-		: inputBox.classList.remove('error');
-	console.log(valLenght);
-
+inputBox.forEach(function(elemento){
+    const text_area = elemento.children[1];
+    const min_num = elemento.children[2].children[0];
+    
+    text_area.addEventListener('keyup',()=>{
+        let valLenght = text_area.value.length; 
+        min_num.innerHTML=valLenght;
+        valLenght>0  
+            ? elemento.classList.add('active') 
+            : elemento.classList.remove('active'); 
+    
+        valLenght>200
+            ? elemento.classList.add('error')
+            : elemento.classList.remove('error');
+        console.log(valLenght);
+    
+    });
 });
-
-
-
 
