c34686e1446a6e1e1972e006fd3b2e471869687e
Pedro Rocha <pedrorocha.bou@gmail.com>
2022-03-21 13:49:09 -0300

    adicionando painel de navegação para paginação

--- publicacoes.php
+++ publicacoes.php
@@ -98,6 +98,23 @@
                     </div>
                 </div>
             </div>
+
+            <nav class="paginacao">
+                <ul>
+                    <li>
+                        <a href="#">1</a>
+                    </li>
+                    <li>
+                        <a href="#">2</a>
+                    </li>
+                    <li>
+                        <a href="#">3</a>
+                    </li>
+                    <li>
+                        <a href="#">4</a>
+                    </li>
+                </ul>
+            </nav>
         </section>
     </main>
 
