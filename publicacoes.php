3909e904788e5db17e9835c308398ff81d3c3ed6
Pedro Rocha <pedrorocha.bou@gmail.com>
2022-03-22 14:00:46 -0300

    adicionando tratamento para quando não há resultados para a busca (publicacoes.php)

--- publicacoes.php
+++ publicacoes.php
@@ -4,7 +4,7 @@ require_once('script/conexao.php');
 //Verifica existência da variável 'pagina_atual'
 if (!empty($_GET['pagina_atual'])) {
     $current_page = $_GET['pagina_atual'];
-}else{
+} else {
     $current_page = 1;
 }
 
@@ -19,12 +19,12 @@ $items_per_page = 5;
 $start_item = $items_per_page * ($current_page - 1);
 
 //Obtendo valores dos inputs através do GET
-if(!empty($_GET['titulo'])){
+if (!empty($_GET['titulo'])) {
     $titulo = '%' . $_GET['titulo'] . '%';
     $autores = '%' . $_GET['autores'] . '%';
     $palavra_chave = '%' . $_GET['palavra-chave'] . '%';
     $ano = '%' . $_GET['ano'] . '%';
-}else{
+} else {
     $titulo = $autores = $palavra_chave = $ano = '%%';
 }
 
@@ -65,6 +65,9 @@ $rows_per_page = $statement->rowCount();
 //Quantidade de páginas necessárias para mostrar os itens
 $pages = ceil($total_rows / $items_per_page);
 
+//Caso não houver nenhum resultado para a pesquisa o número de páginas será 0
+if ($pages == 0) $current_page = 0;
+
 //Caso 'current_page' contiver um valor inválido o usuário será redirecionado para a última página válida
 if ($current_page > $pages) header("Location: publicacoes.php?$url pagina_atual=$pages");
 ?>
@@ -96,89 +99,96 @@ if ($current_page > $pages) header("Location: publicacoes.php?$url pagina_atual=
                 <input id="palavra-chave" class="text-box-default" name="palavra-chave" type="text" placeholder="Digite uma ou mais chaves">
                 <label for="ano-publicacao">Ano de publicação</label>
                 <input id="ano-publicacao" class="text-box-default" name="ano" type="text" placeholder="Digite o ano da publicação">
-                
+
                 <button name="buscar" id="buscar" class="btn-default">
                     <img src="images/bt_lupa.png" alt="Botão buscar">
                 </button>
-                
+
             </form>
         </section>
         <hr id="breakpoint">
         <section id="publicacoes" tabindex="-1">
             <?php
-            
-            foreach ($items as $key => $value) {
+            if ($current_page == 0) {
             ?>
-                <div class="card-default">
-                    <h4>
-                        <?= $value['titulo']; ?>
-                    </h4>
+                <h1 class="no-results">Sem resultados!</h1>
+            <?php
+            } else {
+                foreach ($items as $key => $value) {
+            ?>
+                    <div class="card-default">
+                        <h4>
+                            <?= $value['titulo']; ?>
+                        </h4>
 
-                    <div class="infos">
-                        <div class="descricao">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                        </div>
-                        <div class="compartilhar">
-                            <p>Compartilhe</p>
-                            <div class="redes-sociais">
-                                <img src="images/svg/ic_whatsapp.svg" alt="Whatsapp ícone">
-                                <img src="images/svg/ic_facebook.svg" alt="Facebook ícone">
-                                <img src="images/svg/ic_twitter_blue.svg" alt="Twitter ícone">
+                        <div class="infos">
+                            <div class="descricao">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                            </div>
+                            <div class="compartilhar">
+                                <p>Compartilhe</p>
+                                <div class="redes-sociais">
+                                    <img src="images/svg/ic_whatsapp.svg" alt="Whatsapp ícone">
+                                    <img src="images/svg/ic_facebook.svg" alt="Facebook ícone">
+                                    <img src="images/svg/ic_twitter_blue.svg" alt="Twitter ícone">
+                                </div>
+                            </div>
+                            <div class="autores">
+                                <p>
+                                    <?= $value['autores']; ?>
+                                </p>
                             </div>
                         </div>
-                        <div class="autores">
-                            <p>
-                                <?= $value['autores']; ?>
-                            </p>
+
+                        <div class="download-ano">
+                            <a href="<?= $value['link'] ?>">
+                                <button class="btn-default">Download/Link</button>
+                            </a>
+
+                            <p class="ano-publicacao">Ano de publicação: <?= $value['ano'] ?></p>
                         </div>
                     </div>
+                <?php
+                } //ENDFOREACH
+                ?>
 
-                    <div class="download-ano">
-                        <a href="<?= $value['link'] ?>">
-                            <button class="btn-default">Download/Link</button>
-                        </a>
+                <!-- PAINEL DE PAGINAÇÃO -->
+                <nav class="paginacao">
+                    <ul>
+                        <li>
+                            <a href="publicacoes.php?<?= $url ?>pagina_atual=1">
+                                Primeira
+                            </a>
+                        </li>
 
-                        <p class="ano-publicacao">Ano de publicação: <?= $value['ano'] ?></p>
-                    </div>
-                </div>
+                        <?php
+                        $right_margin = $left_margin = 3;
+                        for ($page = $current_page - $left_margin; $page <= $current_page + $right_margin; $page++) {
+                            if ($current_page == $page) $bt_class = 'active';
+                            else $bt_class = '';
+
+                            if ($page >= 1 && $page <= $pages) {
+                        ?>
+                                <li>
+                                    <a class="<?= $bt_class ?>" href="publicacoes.php?<?= $url ?>pagina_atual=<?= $page ?>">
+                                        <?= $page ?>
+                                    </a>
+                                </li>
+                        <?php
+                            } //ENDIF
+                        } //ENDFOR
+                        ?>
+
+                        <li>
+                            <a href="publicacoes.php?<?= $url ?>pagina_atual=<?= $pages ?>">
+                                Última
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
             <?php
-            } //ENDFOREACH
+            } //ENDIFELSE
             ?>
-
-            <!-- PAINEL DE PAGINAÇÃO -->
-            <nav class="paginacao">
-                <ul>
-                    <li>
-                        <a href="publicacoes.php?<?= $url ?>pagina_atual=1">
-                            Primeira
-                        </a>
-                    </li>
-
-                    <?php
-                    $right_margin = $left_margin = 3;
-                    for ($page = $current_page - $left_margin; $page <= $current_page + $right_margin; $page++) {
-                        if ($current_page == $page) $bt_class = 'active';
-                        else $bt_class = '';
-
-                        if ($page >= 1 && $page <= $pages) {
-                    ?>
-                            <li>
-                                <a class="<?= $bt_class ?>" href="publicacoes.php?<?= $url ?>pagina_atual=<?= $page ?>">
-                                    <?= $page ?>
-                                </a>
-                            </li>
-                    <?php
-                        } //ENDIF
-                    } //ENDFOR
-                    ?>
-
-                    <li>
-                        <a href="publicacoes.php?<?= $url ?>pagina_atual=<?= $pages ?>">
-                            Última
-                        </a>
-                    </li>
-                </ul>
-            </nav>
         </section>
     </main>
 
