ac045ff53da22d774ac7c50398276b0106d3016a
gabrielsilva28 <go.silva@discente.ufma.br>
2022-04-05 12:55:30 -0300

    alteração no texto

--- sobre.php
+++ sobre.php
@@ -18,15 +18,10 @@
         <h2 class="titulo">SOBRE NÓS</h2>
         <section id="sobre">
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                Somos um grupo de pesquisa tecnológica e aplicada que objetiva desenvolver soluções pioneiras nas áreas de Engenharia de Software e Interação Humano-Computador. Desenvolvemos tecnologias de apoio ao processo de desenvolvimento de software com qualidade. Trabalhamos em projetos de software, visando aplicar tecnologias de vanguarda e inovar no processo de desenvolvimento de software, visando qualidade de produto e redução de custos.
             </p>
-            <p>
-                The purpose of lorem ipsum is to create a natural looking block of text (sentence, paragraph, page, etc.) that doesn't distract from the layout. A practice not without controversy, laying out pages with meaningless filler text can be very useful when the focus is meant to be on design, not content.
-                The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software. Today it's seen all around the web; on templates, websites, and stock designs. Use our generator to get your own, or read on for the authoritative history of lorem ipsum.
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
+           
+            
         </section>
         <section id="galeria">
             <div class="imagem img-1"></div>
